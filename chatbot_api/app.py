c45ea608003f8852019881952005d9199f3fecd3
les4975 <les1999@naver.com>
2024-05-26 22:17:09 +0900

    챗봇 새로운 대화 처리 수정

--- chatbot_api/app.py
+++ chatbot_api/app.py
@@ -38,24 +38,25 @@ def chat():
     previous_conversations = Conversation.query.filter_by(user_id=user.id).all()
     conversation_history = "\n".join([f"User: {conv.message}\nBot: {conv.response}" for conv in previous_conversations])
 
-    # 역할 및 content 지정
-    role = "system"
-    content = "당신은 moodTunes의 챗봇입니다. 사용자가 기능을 사용하고 싶다고 질문하면 해당하는 기능을 제공하거나, 사용자의 요청이나 피드백을 수용할 수 있습니다. 되도록 사용자의 질문에 대해 짧고 간결하고 친절하게 대답해주세요."
+ # 역할 및 content 지정
+    system_message = {
+        "role": "system",
+        "content": "당신은 moodTunes의 챗봇입니다. 사용자가 기능을 사용하고 싶다고 질문하면 해당하는 기능을 제공하거나, 사용자의 요청이나 피드백을 수용할 수 있습니다. 되도록 사용자의 질문에 대해 짧고 간결하고 친절하게 대답해주세요."
+    }
 
-    # 처음 대화에서는 챗봇의 역할을 system으로 설정
-    if previous_conversations:
-        role = "assistant"
-        content = user_input
-
-
-    # 새로운 대화 처리
+      # 새로운 대화 처리
     prompt = f"{conversation_history}\nUser: {user_input}\nBot:"
+
     response = client.chat.completions.create(
-        model ="gpt-3.5-turbo",
+        model="gpt-3.5-turbo",
         messages=[
             {
-                "role":role,
-                "content":content
+                "role": "system",
+                "content": "당신은 moodTunes의 챗봇입니다. 사용자가 기능을 사용하고 싶다고 질문하면 해당하는 기능을 제공하거나, 사용자의 요청이나 피드백을 수용할 수 있습니다. 되도록 사용자의 질문에 대해 짧고 간결하고 친절하게 대답해주세요."
+            },
+            {
+                "role": "user",
+                "content": prompt
             }
         ],
         max_tokens=150,
@@ -63,23 +64,13 @@ def chat():
     )
     response_text = response.choices[0].message.content.strip()
 
+
     # 대화 저장
     new_conversation = Conversation(user_id=user.id, message=user_input, response=response_text)
     db.session.add(new_conversation)
     db.session.commit()
 
     return jsonify({"response": response_text})
-
-# def handle_scenario(user_input):
-#     if "날씨" in user_input:
-#         return "현재 날씨는 맑고 기온은 20도입니다."
-#     elif "뉴스" in user_input:
-#         return "오늘의 주요 뉴스 헤드라인: 경제가 회복되고 있습니다."
-#     elif "안녕하세요" in user_input or "안녕" in user_input:
-#         return "안녕하세요! 무엇을 도와드릴까요?"
-#     else:
-#         return get_chatgpt_response(user_input)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
