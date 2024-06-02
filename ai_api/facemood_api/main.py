494969b9fcb755df96a17505765125442ca09d19
Ubuntu <ubuntu@ip-172-26-14-40.ap-northeast-2.compute.internal>
2024-06-02 04:46:02 +0000

    표정인식 db 생성

--- ai_api/facemood_api/main.py
+++ ai_api/facemood_api/main.py
@@ -3,6 +3,7 @@ import io
 import torch
 from PIL import Image
 import torchvision.transforms as T
+import mysql.connector
 from ultralytics import YOLO
 
 app = Flask(__name__)
@@ -10,12 +11,36 @@ app = Flask(__name__)
 # YOLOv8 모델 불러오기
 model = YOLO('best.pt')
 
+# MySQL 연결 설정
+db_config = {
+    'user': 'root',
+    'password': 'capstone02005',
+    'host': 'localhost',
+    'database': 'emotion_db'
+}
+
 # 예측 함수
 def predict(image):
     transform = T.Compose([T.ToTensor()])
     img = transform(image).unsqueeze(0)  # 배치 차원 추가
     results = model(img)
     return results
+    
+    
+# MySQL에 결과 저장 함수
+def save_results_to_db(predictions):
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        add_emotion = ("INSERT INTO emotions (emotion_data) VALUES (%s)")
+        emotion_json = json.dumps(predictions)
+        cursor.execute(add_emotion, (emotion_json,))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("Emotion data inserted successfully.")
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
 
 @app.route('/mood-tunes', methods=['POST'])
 def predict_endpoint():
@@ -31,6 +56,10 @@ def predict_endpoint():
     
     # 결과 반환 (결과를 JSON 형태로 반환)
     predictions = results.pandas().xyxy[0].to_dict(orient='records')  # 예측 결과를 딕셔너리 형태로 변환
+    
+    # 결과를 MySQL에 저장
+    save_results_to_db(predictions)
+    
     return jsonify(predictions)
 
 if __name__ == '__main__':
