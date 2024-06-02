28cfb0cfc2d662c530e22dd4e3e839b07d056c2c
goddessana <twicegoddessana1229@gmail.com>
2024-06-02 16:56:53 +0900

    refactor: 라우트 함수 얇게 유지

--- backend/ai/utils.py
+++ backend/ai/utils.py
@@ -21,3 +21,19 @@ def load_model(model_path):
     """
     model = YOLO(model_path)
     return model
+
+
+def get_prediction(model, image):
+    """
+    주어진 이미지에 대한 예측을 수행하고, 가장 높은 신뢰도를 가진 클래스의 이름을 반환합니다.
+    """
+    results = model(image)
+    predictions = results[0].boxes.data
+    if len(predictions) > 0:
+        top_prediction = predictions[0]
+        top_class_id = int(top_prediction[-1])
+        top_class_name = model.names[top_class_id]
+    else:
+        top_class_name = "unknown"
+
+    return top_class_name
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 
-from ai.utils import transform_image, load_model
+from ai.utils import transform_image, load_model, get_prediction
 from ai.schemas import ImageResponse
 
 app = FastAPI()
@@ -9,24 +9,7 @@ app = FastAPI()
 @app.post('/predict', response_model=ImageResponse)
 async def predict(image: UploadFile = File(...)):
     model = load_model('model/best.pt')
+    image = transform_image(await image.read())
+    emotion = get_prediction(model, image)
 
-    image_bytes = await image.read()
-    image = transform_image(image_bytes)
-
-    # 예측
-    results = model(image)
-
-    # 예측된 클래스 이름과 신뢰도 추출
-    predictions = results[0].boxes.data  # YOLOv8의 결과는 첫 번째 요소의 'boxes' 속성에 있습니다
-    if len(predictions) > 0:
-        top_prediction = predictions[0]  # 첫 번째 예측을 가장 높은 신뢰도로 간주
-        top_class_id = int(top_prediction[-1])  # 클래스 ID는 마지막 열에 있음
-        top_class_name = model.names[top_class_id]  # 클래스 이름 가져오기
-    else:
-        top_class_name = "unknown"
-
-    response = {
-        'emotion': top_class_name
-    }
-
-    return response
+    return {'emotion': emotion}
